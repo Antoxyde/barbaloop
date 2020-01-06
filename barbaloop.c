489b4573a9e8f51a3323f1cf1cb08730bf94e3b9
Antoxyde <Antoxyde@protonmail.com>
2020-01-06 06:38:13 +0100

    first commit

---
+++ barbaloop.c
@@ -0,0 +1,64 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+#include <sys/resource.h>
+
+typedef void (*actualizer)(char*);
+
+typedef struct actualizer_args_ {
+    actualizer func;
+    pthread_mutex_t lock;
+    size_t sleeptime;
+    char* value;
+} actualizer_args;
+
+void actualize_time(char* value) {
+    time_t rawtime;
+    struct tm *info;
+    info = localtime(&rawtime);
+    strftime(value, 80, "%a %x - %X", rawtime);
+}
+
+void actualize_ram(char* value) {
+    struct rusage usage;
+    int err;
+
+    err = getrusage(&usage);
+}
+
+void thread_main(actualizer_args* args) {
+
+    while (1) {
+
+        pthread_mutex_lock(&(args->lock));
+        args->func(args->value);
+        pthread_mutex_unlock(&(args->lock));
+
+        sleep(args->sleeptime);
+    }
+
+}
+
+int main() {
+
+    pthread_mutex_t time_lock;
+    char time_value[80];
+    actualizer_args time_args = {&actualize_time, time_lock, 1, time_value};
+
+    pthread_t time_thread;
+    int err;
+
+    err = pthread_create(&time_thread, NULL, thread_main, &time_args);
+
+    while (1) {
+
+        sleep(1);
+        pthread_mutex_lock(&time_lock);
+        printf("Time: %s\n", time_value);
+        pthread_mutex_unlock(&time_lock);
+    }
+
+    return 0;
+}
+
